413efb298043550c901e046837fb3292405c4592
Chuong Minh <chuongnmps25390@fpt.edu.vn>
2023-10-19 22:08:43 +0700

    next config rebuild

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    staticPageGenerationTimeout: 100,
     reactStrictMode: true,
     images: {
         domains: ['res.cloudinary.com']
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:storeId/billboards/:billboardId',
+                destination: '/api/billboards/:billboardId?store=:storeId',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
